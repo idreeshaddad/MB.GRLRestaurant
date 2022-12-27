2d66eaca7ae2d5d513abb498f91a8faa88395102
Idrees Haddad <gelnox@live.com>
2022-12-27 19:12:44 +0300

    Finished newsletter section with responsive support

---
+++ MB.GRLRestaurant.Web/Models/DriverViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MB.GRLRestaurant.Web.Models
+{
+    public class DriverViewModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int? CarId { get; set; }
+    }
+}
